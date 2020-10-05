22595975baf1a97441561a30d8a71caec6f39388
Maistrenkov Yaroslav <y.maistrenkov@betinvest.com>
2020-10-05 18:41:55 +0300

    converter fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Converter from './Converter/Converter';
+import Converter from 'Converter';
 
 ReactDOM.render(
   <Converter />,
